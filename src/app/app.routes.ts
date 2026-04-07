4aa5c75c08c8846d716b39b8ee9bbd45e1b48701
Lee Forbes <fillthatinfo@gmail.com>
2026-04-06 20:46:35 -0500

    sponsor us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { Schedule } from './schedule/schedule';
 import { Media } from './media/media';
 import { SummerCamp } from './summer-camp/summer-camp';
+import { Sponsors } from './sponsors/sponsors';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'schedule', component: Schedule },
     { path: 'media', component: Media },
     { path: 'summer-camp', component: SummerCamp },
+    { path: 'sponsors', component: Sponsors },
 ];
